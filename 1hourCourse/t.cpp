14a672cd82b50f57d64b2e292a2edbd2136b239a
simingw2209163 <simingw2209@163.com>
2025-03-01 04:54:39 +0000

    setup

---
+++ 1hourCourse/t.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//===============initialize variable
+// int main(){
+//     int file_size = 100;
+//     double two_decimal = 9.99;
+//     const constant = 10; // consrant which will not change, also can not be overwritten
+//     cout << "Hello World!";
+//     return 0;
+// }
+
+//===============swap A and B
+// int main(){
+//     int a = 1;
+//     int b = 2;
+//     ////let temp store a,so a is empty,then let a = b, then b is empty
+//     int temp = a;
+//     a = b;
+//     b = temp;
+//     cout << b;
+// }
+
+//======Mathematical Expression
+ int main(){
+    int x = 10;
+    int y = x++;  //cout x = 11, cout y =10
+    int z = ++x;  //cout x = 11, cout z = 11
+    cout << z;
+
+ }
